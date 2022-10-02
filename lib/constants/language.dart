9c69f42e6d5cae3fd8e9a4e49d6d5b16082b54f0
Stefano Miceli <s.miceli90@gmail.com>
2022-10-02 16:32:10 +0200

    Con languages

---
+++ lib/constants/language.dart
@@ -0,0 +1,46 @@
+
+
+
+String? getCurrentLanguageValue(String field){
+  // una cosa per capire in che lingua è il telefono
+  // cerca la stringa in base a quella lingua
+  return LANG_IT[field];
+}
+
+
+const String DASHBOARD_CONST = "DASHBOARD";
+
+
+
+
+final Map<String,String> LANG_IT = {
+  DASHBOARD_CONST:"Dashboard",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+};
+
+final Map<String,String> LANG_EN = {
+  DASHBOARD_CONST:"Dashboard",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+  "":"",
+};
