f64764b4e94e16ee94166cafd216d04f3ee53c40
Stefano <s.miceli90@gmail.com>
2022-12-19 19:41:44 +0100

    Some corrections

---
+++ lib/widgets/utilities/image_utility.dart
@@ -0,0 +1,26 @@
+
+
+
+import 'package:flutter/material.dart';
+
+Widget getImageFromName(String name) {
+  return Image.asset("assets/images/$name");
+}
+
+
+class ImagePlaceholder extends StatelessWidget {
+
+  final double width;
+  final double height;
+
+  final String name;
+
+  const ImagePlaceholder({Key? key, this.width = 150, this.height = 150, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     return Image.asset("assets/images/$name", width: width, height: height);
+  }
+
+
+}
